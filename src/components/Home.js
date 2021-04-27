fa68acf6241342637c59c3cad2bc602428cf3f0b
Cody Thigpen <ckthigpen1@gmail.com>
2021-04-27 15:48:18 -0400

    added styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,18 +1,21 @@
 import React from 'react';
-import miamiPic from '../resources/miami.png';
+import Party from '../resources/party.jpeg';
+import Logo from '../resources/logo.png';
 import { Link } from 'react-router-dom';
-import Places from './Places'
+import Places from './Places';
+import './style.css';
 
-const Home = () =>{
+const Home = () => {
     console.log(Places)
     return (
-        
-    <Link to="/miami">
-        <article>
-            <img src={miamiPic} alt="Miami Beach"></img>
-            <h2>Miami</h2>
-        </article>
-    </Link>
+
+        <Link to="/miami">
+            <article>
+                <img className='homeImg' src={Party} alt="party"></img>
+                <img className='logo' src={Logo} alt="logo"></img>
+                <h2>Miami</h2>
+            </article>
+        </Link>
     )
 }
 
