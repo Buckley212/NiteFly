6a1c0d045d992155e4b0a4a96e5fb418f431183d
Cody Thigpen <ckthigpen1@gmail.com>
2021-04-27 16:14:30 -0400

    home

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Party from '../resources/party.jpeg';
 import Logo from '../resources/logo.png';
 import { Link } from 'react-router-dom';
-import Places from './Places';
+// import Places from './Places';
 import './style.css';
 
 
@@ -11,9 +11,17 @@ const Home = () => {
 
         <Link to="/miami">
             <article>
+
                 <img className='homeImg' src={Party} alt="party"></img>
-                <img className='logo' src={Logo} alt="logo"></img>
-                <h2>Miami</h2>
+
+                <div className='flex'>
+                    <div><img className='logo' src={Logo} alt="logo"></img></div>
+                    <div><h2>Miami</h2></div>
+                    <div><h2>New York</h2></div>
+                    <div><h2>Los Angeles</h2></div>
+                    <div><h2>Dallas</h2></div>
+
+                </div>
             </article>
         </Link>
     )
