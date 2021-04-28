b45469b297e9d1c58003ff6e0cc573e851f15a7b
Cody Thigpen <ckthigpen1@gmail.com>
2021-04-28 16:56:15 -0400

    each place

--- src/App.js
+++ src/App.js
@@ -4,22 +4,22 @@ import Home from "./components/Home";
 import Miami from "./cities/Miami";
 import SanFrancisco from "./maps/SanFranciscoMap";
 import NavBar from "./components/NavBar";
-// import EachPlace from './components/EachPlace'
+import EachPlace from './components/EachPlace'
 
 function App() {
-	return (
-		<div className="App">
-			<NavBar />
-			<Switch>
-				<Route exact path="/" component={Home} />
-				<Route exact path="/miami" component={Miami} />
-				<Route exact path="/sanfran" component={Miami} />
-				<Route exact path="/ny" component={Miami} />
-				<Route exact path="/dallas" component={Miami} />
-				{/* <Route exact path='/each-place' component={EachPlace} /> */}
-			</Switch>
-		</div>
-	);
+  return (
+    <div className="App">
+      <NavBar />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/miami" component={Miami} />
+        <Route exact path="/sanfran" component={Miami} />
+        <Route exact path="/ny" component={Miami} />
+        <Route exact path="/dallas" component={Miami} />
+        <Route exact path='/each-place' component={EachPlace} />
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
