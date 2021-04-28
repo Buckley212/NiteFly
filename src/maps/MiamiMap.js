cb3f0f409244a3ac056fdbd6d7ab663082c6a8ce
Buckley212 <rob36611@gmail.com>
2021-04-27 21:42:16 -0400

    removed grabber cursor when hovering map

--- src/maps/MiamiMap.js
+++ src/maps/MiamiMap.js
@@ -1,14 +1,12 @@
 import React, { useState, useEffect, Component } from 'react';
 import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
 import mapStyle from './mapStyle.json';
-import Markers from '../components/Markers';
 import axios from 'axios';
 
 
 
 const MapContainer = (props) => {
     const [markers, setMarkers] = useState([])
-      
     useEffect (() => {
     axios.get('https://iron-cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=25.76439676537,-80.20731679740429&radius=1500&type=restaurant&key=AIzaSyBibnOWEr72nhfg0dEPgv5Amv09pXcRk_M')
     .then((response) => {
@@ -21,13 +19,14 @@ const MapContainer = (props) => {
         <Map
           className='Map'
           google={props.google}
-          zoom={13}
+          zoom={14.5}
+          draggableCursor='default'
           styles={mapStyle}
-          // disableDefaultUI = {true}
-          // zoomControl = {false}
-          // scaleControl = {false}
-          // rotateControl = {false}
-          // gestureHandling = "none"
+          disableDefaultUI = {true}
+          zoomControl = {false}
+          scaleControl = {false}
+          rotateControl = {false}
+          gestureHandling = "none"
           initialCenter={
             {
               lat: 25.76439676537,
