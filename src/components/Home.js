aba11f90bc5c4e19173e9d0457e2c2fe3b573aec
Joe <joeyballesteros.wfg@gmail.com>
2021-04-29 11:02:20 -0700

    just pulling

--- src/components/Home.js
+++ src/components/Home.js
@@ -25,9 +25,6 @@ const Home = () => {
                     <Link className='linkColor middle focus' to="/dallas">
                         <h2>Dallas</h2>
                     </Link>
-                    <Link className='linkColor middle focus' to="/about">
-                        <h2>About</h2>
-                    </Link>
                 </div>
             </article>
         </div>
