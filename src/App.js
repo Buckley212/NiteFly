92ae17dbb9be90e4bf184d46c7d64cb882bbafe3
Joe <joeyballesteros.wfg@gmail.com>
2021-04-29 06:04:12 -0700

    refresh

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 import Home from "./components/Home";
-import Miami from "./cities/Miami/Miami";
-import SanFrancisco from "./cities/SanFrancisco/SanFranciscoMap";
+import Miami from "./cities/Miami/MiamiMap";
 import NavBar from "./components/NavBar";
 import EachPlace from './cities/Miami/EachPlace'
 
 function App() {
   return (
     <div className="App">
-      <NavBar />
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/miami" component={Miami} />
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,6 +2,9 @@ import React from 'react';
 import './NavBar.css'
 import { Link } from 'react-router-dom'; 
 
+// const [isOpen, setIsOpen] = useState(false);
+// const toggle => setIsOpen(!isOpen)
+
 function NavBar() {
     return (
         <div>
