cf487de73ca38c6fcde29fd6e992ac1a6a3e895d
Buckley212 <rob36611@gmail.com>
2021-04-30 13:13:58 -0400

    added likes property and sorting

--- src/cities/SanFrancisco/PlacesList.js
+++ src/cities/SanFrancisco/PlacesList.js
@@ -29,18 +29,31 @@ const PlacesList = () => {
         )
         return setLocations(sortedList2)
     };
+    const sortByLikes = () => {
+        let sortedList3 = [...locations]
+        sortedList3.sort((a, b) => {
+            return b.props.likes - a.props.likes
+            }
+        )
+        return setLocations(sortedList3)
+    };
     useEffect(() => {
         axios.get('https://iron-cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=37.748898693450016,-122.43793300701175&radius=1500&type=bar&key=AIzaSyBibnOWEr72nhfg0dEPgv5Amv09pXcRk_M')
             .then((response) => {
                 console.log(response.data.results);
-                setLocations(response.data.results.map(place => <section className="Location ani focus" name={place.name} rating={place.rating} price={place.price_level} key={place.place_id} title={place.name} >
-                    <Link to={`/each-place-sf/${place.place_id}`}>
-                        <h4>{place.name}</h4>
-                    </Link>
-                    <span>{place.vicinity}</span>
-                    <p>{place.rating}<br />{'★'.repeat(Math.floor(place.rating))}{'☆'.repeat(~(Math.floor(place.rating) - 5) + 1)}</p>
-                    <p className="price">{'$'.repeat(place.price_level)}</p>
-                </section>))
+                setLocations(response.data.results.map(place => {
+                    const likesNum = Math.floor(Math.random()*1000)
+                    return (
+                        <section className="Location ani focus" name={place.name} likes={likesNum} rating={place.rating} key={place.place_id} price={place.price_level} title={place.name} >
+                        <Link to={`/each-place/${place.place_id}`}>
+                            <h4>{place.name}</h4>
+                        </Link>
+                        <span>{place.vicinity}</span>
+                        <p>{place.rating}<br />{'★'.repeat(Math.floor(place.rating))}{'☆'.repeat(~(Math.floor(place.rating) - 5) + 1)}</p>
+                        <p className="price">{'$'.repeat(place.price_level)}</p>
+                        <p className="likes">👍  {likesNum}</p>
+                    </section>)
+                }))
             })
     }, []);
     return (
@@ -50,6 +63,7 @@ const PlacesList = () => {
             <button onClick={() => sortByRating()}>Rating</button>
             <button onClick={() => sortByName()}>Name</button>
             <button onClick={() => sortByPrice()}>Price</button>
+            <button onClick={() => sortByLikes()}>Likes</button>
             </div>
             {locations}
         </div>
