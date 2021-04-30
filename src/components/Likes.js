054e182f721049844d171578f6f21fd26a00e2da
Buckley212 <rob36611@gmail.com>
2021-04-30 14:29:34 -0400

    randomized likes and added button functionality

--- src/components/Likes.js
+++ src/components/Likes.js
@@ -2,8 +2,8 @@ import React, {useState} from 'react';
 
 
 function Likes() {
-    const likes1 = Math.floor(Math.random() * 100);
-    const likes2 = Math.floor(Math.random() * 100);
+    const likes1 = (Math.floor(Math.random() * 1000)+1000);
+    const likes2 = (Math.floor(Math.random() * 1000)+10);
 
     let [like1, setLike1] = useState(likes1-1);
     let [like2, setLike2] = useState(likes2-1);
@@ -15,10 +15,10 @@ function Likes() {
                 <h5>Did this place satisfy your munchies?</h5>
             </div>
             <div>
-                <button className='likesBtn' onClick={() => like1 < likes1 && like2 < likes2?  setLike1(++like1): setLike1(like1)}>
+                <button className='likesBtn' onClick={() => like1 < likes1+1 && like2 < likes2?  setLike1(++like1): setLike1(like1)}>
                     <p style={{color}}>Yum Yum!</p> {like1} 
                 </button>
-                <button className='likesBtn' onClick={() => like2 < likes1 && like1 < likes2 ?  setLike2(++like2): setLike2(like2)}>
+                <button className='likesBtn' onClick={() => like2 < likes2 && like1 < likes1?  setLike2(++like2): setLike2(like2)}>
                     <p style={{color}}>Hell Nah!</p> {like2} 
                 </button>
             </div>
